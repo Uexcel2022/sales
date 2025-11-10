0b201118ff7b45226af6b834f09e743f2856150e
Uexcel2022 <excellenceudoka@gmail.com>
2025-11-10 23:21:21 +0100

    added delivery partner endponts

--- dependency.py
+++ dependency.py
@@ -1,13 +1,14 @@
 
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
-from service.auth import AuthService
+from service.seller import SellerService
 from service.shipment import ShipmentService
+from service.delivary_partner import DeliverPartnerService
 from session import get_session
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
-pwd_bearer = OAuth2PasswordBearer(tokenUrl="/api/v1/token")
+pwd_bearer = OAuth2PasswordBearer(tokenUrl="/api/v1/seller/token")
 
 sesseionDep = Annotated[AsyncSession,Depends(get_session)]
 
@@ -15,13 +16,19 @@ def get_shipment_service(session:sesseionDep):
     return ShipmentService(session)
 
 def get_seller_service(session:sesseionDep):
-    return AuthService(session)
+    return SellerService(session)
+
+def get_delivery_partner_service(session:sesseionDep):
+    return DeliverPartnerService(session)
+
 
 
 
 shipmentServiceDep = Annotated[ShipmentService, Depends(get_shipment_service)]
 
-authServiceDep = Annotated[AuthService, Depends(get_seller_service)]
+deliveryPartnerServiceDep = Annotated[DeliverPartnerService, Depends(get_delivery_partner_service)]
+
+sellerServiceDep = Annotated[SellerService, Depends(get_seller_service)]
 
 pwd_bearerDP = Annotated[str, Depends(pwd_bearer)]
 
